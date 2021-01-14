faacbb3d4ae01b881a809a793d6f8724b5412b7d
shindje <aleks.shindje@gmail.com>
2021-01-14 23:08:13 +0300

    Picture of the day

---
+++ app/src/main/java/com/example/gb_material/PictureOfTheDayFragment.kt
@@ -0,0 +1,60 @@
+package com.example.gb_material
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import coil.api.load
+import com.example.gb_material.web.PictureOfTheDayData
+import kotlinx.android.synthetic.main.pod_fragment.*
+
+class PictureOfTheDayFragment: Fragment() {
+    private val viewModel: PictureOfTheDayViewModel by lazy {
+        ViewModelProviders.of(this).get(PictureOfTheDayViewModel::class.java)
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.pod_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.getData().observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+    }
+
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    Toast.makeText(requireContext(), "URL картинки пустой", Toast.LENGTH_SHORT).show()
+                } else {
+                    //Отобразите фото
+                    //showSuccess()
+                    //Coil в работе: достаточно вызвать у нашего ImageView
+                    //нужную extension-функцию и передать ссылку и заглушки для placeholder
+                    eiv_pod.load(url) {
+                        lifecycle(this@PictureOfTheDayFragment)
+                        error(R.drawable.ic_baseline_image_not_supported_24)
+                        placeholder(R.drawable.ic_baseline_sync_24)
+                    }
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+                //Отобразите загрузку
+                //showLoading()
+            }
+            is PictureOfTheDayData.Error -> {
+
+            }
+        }
+    }
+}
