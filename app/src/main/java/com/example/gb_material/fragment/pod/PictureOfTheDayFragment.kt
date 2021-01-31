8b6be70d888c5b1a58ac93bbc16dabceaeba8402
shindje <aleks.shindje@gmail.com>
2021-01-31 20:36:15 +0300

    Transitions

--- app/src/main/java/com/example/gb_material/fragment/pod/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/gb_material/fragment/pod/PictureOfTheDayFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.transition.TransitionManager
 import coil.api.load
 import com.example.gb_material.R
 import com.example.gb_material.fragment.view_pager.PODViewPagerFragment
@@ -91,8 +92,9 @@ class PictureOfTheDayFragment(var date: String?, var viewPagerFragment: PODViewP
                             error(R.drawable.ic_baseline_image_not_supported_24)
                             placeholder(R.drawable.ic_baseline_sync_24)
                             target {
-                                eiv_motion_layout.transitionToStart()
                                 eiv_pod.setImageDrawable(it)
+                                TransitionManager.beginDelayedTransition(eiv_layout)
+                                eiv_layout.visibility = View.VISIBLE
                             }
                         }
                     }
@@ -105,7 +107,9 @@ class PictureOfTheDayFragment(var date: String?, var viewPagerFragment: PODViewP
     }
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
-        eiv_motion_layout.transitionToEnd()
+        TransitionManager.beginDelayedTransition(eiv_layout)
+        eiv_layout.visibility = View.GONE
+
         val calendar = Calendar.getInstance()
         calendar.set(year, month, day)
         val date = sdf.format(calendar.time)
